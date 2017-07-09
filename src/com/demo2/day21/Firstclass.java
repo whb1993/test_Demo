bfa1a94174fccc89cdda6bc037e3beb9ea7162b2
whb1993 <972815205@qq.com>
2017-07-09 21:33:51 +0800

    测试分支

--- src/com/demo2/day21/Firstclass.java
+++ src/com/demo2/day21/Firstclass.java
@@ -6,7 +6,7 @@ package com.demo2.day21;
 public class Firstclass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("实现");
+        System.out.println("添加分支");
 
 
     }
