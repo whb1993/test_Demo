98dcb6d45d51a269a4516d88ca19c2d57f076ae9
whb <tset>
2017-07-09 21:09:41 +0800

    台式机第一次提交代码

---
+++ src/com/demo2/day21/Firstclass.java
@@ -0,0 +1,10 @@
+package com.demo2.day21;
+
+/**
+ * Created by Administrator on 2017/7/9.
+ */
+public class Firstclass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
